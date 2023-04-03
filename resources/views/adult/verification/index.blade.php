28c06ea92c3ea6c8b2c04fb0e7d201e939c709b2
Harish Kumar <harish.dhiman87@gmail.com>
2023-04-03 09:57:43 +0530

    my chnages to ver

--- resources/views/adult/verification/index.blade.php
+++ resources/views/adult/verification/index.blade.php
@@ -29,158 +29,18 @@
             </div>
         </div>
     </div>
-    @if(isset($verification->id))
+   
     <!-- Content Section Start -->
     <div class="relationshipContent">
         <div class="container">
-            <div class="row">
-                <div class="col-sm-12">
-                    <h1>Vocabulary Verification</h1>
-                    <div class="relationImage text-center">
-                        <img src="{{ asset("assets-new/verification_types/" . @$verificationType->banner)}}" alt="relationImage" />
-                        
-                    </div>
-                    <p>{{ @$verificationType->explanation }}</p>
-                </div>
-                <div class="principleRelation">
-                    <div class="conditionBlock">
-                        <div class="blockProblem">
-                            <div class="projectBlock text-center">
-                                <h2>Problem</h2>
-                                <div class="projectList text-center">
-                                    <div class="imgWrp">
-                                        <img class="mx-auto"
-                                            src="{{ asset('assets-new/problem/'.$problem->file)}}" width="100%"
-                                            height="128px">
-                                    </div>
-                                    <p class="redText">{{ $problem->name }}</p>
-                                </div>
-                                <div class="projectList">
-                                    <p class="date">{{ date('d/m/Y', strtotime($problem->file))}}</p>
-                                    <ul class="space">&nbsp;&nbsp;&nbsp;&nbsp;</ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="long-arrow">
-                            <!-- <p style="position:relative; top:35px;left:23px;">is replaced by</p> -->
-                            <!-- add arrow Image over here -->
-                            <img src="{{ asset('assets-new/images/arrowRight.png')}}">
-                            <!-- add arrow Image over here -->
-                        </div>
-                        <div class="blockProblem">
-                            <div class="projectBlock text-center">
-                                <h2>Solution</h2>
-                                <div class="projectList text-center">
-                                    <div class="imgWrp">
-                                        <img class="mx-auto"
-                                            src=" {{ asset('assets-new/solution/'.$solution->file)}}" width="100%"
-                                            height="128px">
-                                    </div>
-                                    <p class="redText">{{ $solution->name }}</p>
-                                </div>
-                                <div class="projectList">
-                                    <p class="date">{{ date('d/m/Y', strtotime($solution->file))}}</p>
-                                    <ul class="space">&nbsp;&nbsp;&nbsp;&nbsp;</ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="long-arrow">
-                            <!-- <p style="position:relative; top:35px;left:25px;">through</p> -->
-                            <!-- add arrow Image over here -->
-                            <img src="{{ asset('assets-new/images/arrowRight.png') }}">
-                            <!-- add arrow Image over here -->
-                        </div>
-                        <div class="blockProblem">
-                            <div class="projectBlock text-center">
-                                <h2>Verification</h2>
-                                <div class="projectList text-center">
-                                    <div class="imgWrp">
-                                        <img class="mx-auto" src="http://127.0.0.1:8000/assets-new/solFunction/1679157286.png"
-                                            width="100%" height="128px">
-                                    </div>
-                                    <p class="redText">Name test asassas</p>
-                                </div>
-                                <div class="projectList">
-                                    <p class="date">18/03/2023</p>
-                                    <ul>
-                                        <li>
-                                            <a href="javaScript:Void(0)" class="editSolFunBtn" data-id="1"
-                                                data-problem="1" data-solution="1" data-file="1679157286.png"
-                                                data-name="Name test asassas" data-solution_function_type_id="1">
-                                                <img src="http://127.0.0.1:8000/assets-new/images/editIcon.png" alt="">
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a data-id="1" class="delSolFunBtn" title="Delete">
-                                                <img src="http://127.0.0.1:8000/assets-new/images/deleteIcon.png"
-                                                    alt=""></a>
-                                        </li>
-                                        <li>
-                                            <a href="#"><img
-                                                    src="http://127.0.0.1:8000/assets-new/images/uploadIcon.png"
-                                                    alt=""></a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="questionWrap">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
-                            tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
-                            nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                            Duis autem vel eum iriure dolor in hendrerit in vulputate velit</p>
-                        <div class="row">
-                            <div class="title">
-                                <h2>Vacabulary</h2>
-
-                            </div>
-                            <div class="entity">
-                                <table class="table slp-tbl text-center">
-                                    <thead>
-                                        <th>Word</th>
-                                        <th>Actual Entity</th>
-                                        <th>Action</th>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Wrod</td>
-                                            <td>Entity</td>
-                                            <td>
-                                                <a href="javaScript:Void(0)" class="editSolFunBtn">
-                                                    <img src="{{ asset('assets-new/images/add-verification.png')}}"
-                                                        alt="">
-                                                </a>
-                                                <a href="javaScript:Void(0)" class="editSolFunBtn">
-                                                    <img src="{{ asset('assets-new/images/deleteIcon.png')}}" alt="">
-                                                </a>
-                                                <a href="javaScript:Void(0)" class="editSolFunBtn">
-                                                    <img src="{{ asset('assets-new/images/editIcon.png')}}" alt="">
-                                                </a>
-
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-
-                        <h2>Validation Question</h2>
-                        <h3>Do you understand the relationship between communication and principle in a project? </h3>
-                        <ul>
-                            <li>Yes, I do understand the relationship between communication and principle in a project.
-                            </li>
-                            <li>No, I do not understand the relationship between communication and principle in a
-                                project. </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            @if(@$verification->id)
+                @include('adult.verification.verification-content')
+            @endif
         </div>
     </div>
     <!-- Content Section End -->
 
-    @else
+    @if(!@$verification->id)
     <div class="relationshipContent" style="height: 280px;">
         <div class="container">
             <div class="row">
@@ -191,97 +51,11 @@
             </div>
         </div>
     </div>
-    
     @endif
-
+   
+    @include('adult.verification.modal.add-verification')
     
-    <!-- Modal Start -->
-    <div class="modal fade" id="createVerification" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" enctype="multipart/form-data">
-
-    <form method="POST" id="createVerificationForm"  enctype="multipart/form-data">
-        @csrf
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title">Add Verification</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                </div>
-                <?php 
-               
-                            $problem_id =  Crypt::encrypt($problem_id);
-                            $project_id =  Crypt::encrypt($project_id);
-                            $solution_id =  Crypt::encrypt($solution_id);
-                        ?>
-                <div class="modal-body">
-
-                    <input type="hidden" name="problem_id" id="problem_id" value="{{ $problem_id }}">
-                    <input type="hidden" name="project_id" value="{{ $project_id }}">
-                    <input type="hidden" name="solution_id" id="solution_id" value="{{ $solution_id }}">
-                    <input type="hidden" name="solution_fun_id" id="solution_fun_id" value="{{ $Solution_function->id }}">
-                    <input type="hidden" name="fileType" id="fileType">
-                    <input type="hidden" name="verificationType" id="verificationType" value="{{ @$verificationType->id }}">
-                    <div class="form-group">
-						<div class="row">
-							<div class="col-6">
-								<div class="custom-control custom-radio mb-5">
-									<input type="radio" id="file" name="file" class="custom-control-input filetypeRadio" value="0" checked>
-									<label class="custom-control-label" for="file"> File</label>
-								</div>
-							</div>
-							<div class="col-6">
-								<div class="custom-control custom-radio mb-5">
-									<input type="radio" id="link" name="file" class="custom-control-input filetypeRadio" value="2">
-									<label class="custom-control-label" for="link"> Link</label>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="form-group" id="imageFile">
-						<input type="file" name="imageFile" id="imageFile" class="dropify" accept="image/*, video/*">
-						
-					</div>
-					<div class="form-group" id="youtubeLink" style="display: none">
-						<input type="url" name="youtubeLink" id="youtubeLink" class="form-control" placeholder="Link">
-						
-					</div>
-                    <div class="form-group">
-
-                    <input type="text"  value="{{ 'Problem : '.@$problem->name }}"  class="form-control" placeholder="Problem Name">
-                        
-                    </div>
-                    <div class="form-group">
-                    <input type="text"  value="{{ 'Solution : '.@$solution->name }}"   class="form-control" placeholder="Problem Name">
-                    </div>
-                    <div class="form-group">
-                        <input type="text"   value="{{ 'Solution Function : '.@$Solution_function->name }}"  class="form-control" id="updateSolFunctionName" placeholder="Solution Function Name *" required>
-                        
-                    </div>
-                    <div class="form-group">
-                        <input type="text" name="varificationName"  value=""  class="form-control" id="varificationName" placeholder="Name *" required>
-                        
-                    </div>
-                    <div class="form-group">
-                    <input type="text"  value="{{ 'varification Type : '.@$verificationType->name }}"  class="form-control" id="varificationType" placeholder="varification Type *" required>
-                    </div>
-                    <div class="form-group">\
-                        @if(isset($verifiationTypeText))
-                        <select class="form-control" name="verification_type_text_id" id="verification_type_text_id">
-                                <option value=''>Choose verification type text..</option>
-                                @foreach($verifiationTypeText as $typetext)
-                                    <option value='{{ $typetext->id }}'>{{ $typetext->name }}</option>
-                                @endforeach
-                        </select>
-                        @endif
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button"   class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" id="btnSave" class="btn btn-success">Save changes</button>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
+    
     <!-- Modal End -->
 </div>
 
@@ -376,6 +150,13 @@ $('.validation').on('change',function(){
         }
         $('#createVerification').modal('toggle')
    })
+//.editSolFunBtn
+
+$('.editverBtn').click(function(){
+   $('#createVerification').modal('toggle')
+})
+
+
 
    $('.filetypeRadio').change(function(){
         var type = $(this).val()
